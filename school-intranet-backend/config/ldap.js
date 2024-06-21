2fca20504838a2a1408ce0903f9db7f218cb8454
Maxime <maxime.bourquard.2005@gmail.com>
2024-06-22 00:37:05 +0200

    Initial commit

--- school-intranet-backend/config/ldap.js
+++ school-intranet-backend/config/ldap.js
@@ -1,10 +1,40 @@
-module.exports = {
-    server: {
-      url: 'ldap://172.20.50.50',
-      bindDn: 'CN=Administrateur,CN=Users,DC=home,DC=local',
-      bindCredentials: 'Bmw1999',
-      searchBase: 'DC=home,DC=local',
-      searchFilter: '(sAMAccountName={{username}})',
-    }
-  };
-  
+const ldap = require('ldapjs');
+
+const client = ldap.createClient({
+  url: process.env.LDAP_URL
+});
+
+const opts = {
+  filter: '(uid=user)',
+  scope: 'sub',
+  attributes: ['dn', 'sn', 'cn']
+};
+
+client.bind(process.env.LDAP_BIND_DN, process.env.LDAP_BIND_PASSWORD, (err) => {
+  if (err) {
+    console.error('Error binding to LDAP:', err);
+  } else {
+    client.search(process.env.LDAP_SEARCH_BASE, opts, (err, res) => {
+      if (err) {
+        console.error('Error searching LDAP:', err);
+      }
+
+      res.on('searchEntry', (entry) => {
+        console.log('Entry:', entry.object);
+      });
+
+      res.on('error', (err) => {
+        console.error('Search error:', err);
+      });
+
+      res.on('end', (result) => {
+        console.log('Search result:', result);
+        client.unbind((err) => {
+          if (err) {
+            console.error('Error unbinding:', err);
+          }
+        });
+      });
+    });
+  }
+});
